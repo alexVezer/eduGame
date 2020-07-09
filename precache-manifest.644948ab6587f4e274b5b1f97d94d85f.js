ef00551b2f106de5e07c732ec372f8d0e3a894b8
alexVezer <alexartimon@gmail.com>
2020-07-09 19:51:40 +0300

    Updates

---
+++ precache-manifest.644948ab6587f4e274b5b1f97d94d85f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac41a92fe283e714a8e21c21f255eaf",
+    "url": "/eduGame/index.html"
+  },
+  {
+    "revision": "7d29a527ba8c4e331db5",
+    "url": "/eduGame/static/css/main.0ea21dba.chunk.css"
+  },
+  {
+    "revision": "6dd63f0d201202803226",
+    "url": "/eduGame/static/js/2.dce38d7d.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/eduGame/static/js/2.dce38d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d29a527ba8c4e331db5",
+    "url": "/eduGame/static/js/main.cac32a00.chunk.js"
+  },
+  {
+    "revision": "6f32d117f59293243fd5",
+    "url": "/eduGame/static/js/runtime-main.34e1a767.js"
+  }
+]);
